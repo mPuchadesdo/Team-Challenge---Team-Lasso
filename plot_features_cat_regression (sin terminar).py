1477717a4f5093141321671efb86fa70e7a98ba2
mPuchadesdo <88624129+mPuchadesdo@users.noreply.github.com>
2025-01-21 22:51:05 +0100

    Add files via upload

--- plot_features_cat_regression (sin terminar).py
+++ plot_features_cat_regression (sin terminar).py
@@ -23,6 +23,21 @@ def plot_features_cat_regression(dataframe, target_col = "", columns = [], pvalu
         tipo: Descripción de lo que retorna la función. COMPLETAR
         """
 
+    sns.set_style("whitegrid")
+    
+    if target_col not in dataframe.columns: # Control para ver si la target_col existe en el dataframe
+        print(f"La columna '{target_col}' no existe en el dataframe")
+        return None
+    
+    if dataframe[target_col].dtype != "float64": # Control para ver si la target_col es una variable numérica continua
+         print(f"La columna '{target_col}' no es una variable numérica continua")
+         return None
+    
+    for col in columns:
+         if col not in dataframe.columns: # Control para ver si las columnas introducidas en columns existen en el dataframe
+              print(f"La columna '{col}' no existe en el dataframe")
+              return None
+    
     sig_num_col = []
     sig_cat_col = []
 
@@ -31,17 +46,23 @@ def plot_features_cat_regression(dataframe, target_col = "", columns = [], pvalu
         for col in dataframe.describe().columns.to_list():
             if len(dataframe[col].unique()) > 12: # Verificamos que no sea una categórica codificada numéricamente
                 num_col.append(col)
+        if dataframe[num_col].isna().sum().sum() != 0: # Verificamos si existen nulos para evitar errores al utilizar la correlación de pearson
+             print(f"Error: existen nulos o NaN presentes en las variables numéricas de estudio")
+             return None
+
         for col in num_col: # Usamos la correlación de pearson para ver si están relacionadas:
             p = pearsonr(dataframe[target_col], dataframe[col]).pvalue
             if p < pvalue: # Si la relación entre variables entra en la significación seleccionada, nos guardamos esa columna:
                 sig_num_col.append(col)
+        if target_col in sig_num_col: # Como nuestro target es una variable numérica, la quitamos de la lista
+            sig_num_col.remove(target_col)
+        print(f"Las columnas numéricas elegidas son: {sig_num_col}")
 
     else:
         for col in columns:
             cat = dataframe[col].unique()
             if len(cat) < 2:
                 continue
-
             if len(cat) == 2: # Si la categoría es binaria, utilizamos el test T de Student
                 group0 = dataframe.loc[dataframe[col] == cat[0], target_col]
                 group1 = dataframe.loc[dataframe[col] == cat[1], target_col]
@@ -49,13 +70,52 @@ def plot_features_cat_regression(dataframe, target_col = "", columns = [], pvalu
             else: # Si la columna tiene más de dos categorías, utilizamos ANOVA:
                 groups = [dataframe.loc[dataframe[col] == c, target_col] for c in cat]
                 p = f_oneway(*groups).pvalue
-
             if p < pvalue: # Si la relación entre variables entra en la significación seleccionada, nos guardamos esa columna:
-                sig_cat_col.append(col)
+                    sig_cat_col.append(col)
+        print(f"Las columnas categóricas elegidas son: {sig_cat_col}")
     
-    if sig_cat_col != []:
-        for cat in sig_cat_col:
-                # Crea el gráfico
-                plt.figure(figsize=(15, 8))
+    if sig_num_col != []:
+        if with_individual_plot:
+            for col in sig_num_col:
+                    # Crea el gráfico
+                    plt.figure(figsize=(12, 8))
+                    sns.scatterplot(x = target_col, y = col, data = dataframe)
+                    plt.title(f"Relación entre {col} y {target_col}")
+                    plt.xlabel(col)
+                    plt.ylabel(f"{col}")
+                    plt.show();
+
+        else:
+            subplots = len(sig_num_col) # Guardamos las filas que va a tener nuestra figura
+            plt.figure(figsize=(20,15))
+            for i, col in enumerate(sig_num_col):
+                plt.subplot(subplots, 1, i+1) # Colocamos cada columna en una de las filas de nuestra figura
+                sns.scatterplot(x = target_col, y = col, data = dataframe)
+                plt.title(f"Relación entre {col} y {target_col}")
+                plt.xlabel(col)
+                plt.ylabel(f"{col}")
+            plt.tight_layout()
+            plt.show();
+    
+    elif sig_cat_col != []:
+        if with_individual_plot:
+            for cat in sig_cat_col:
+                    # Crea el gráfico
+                    plt.figure(figsize=(12, 8))
+                    sns.histplot(x = target_col, hue = cat, data = dataframe, kde = True)
+                    plt.title(f"Relación entre {cat} y {target_col}")
+                    plt.xlabel(target_col)
+                    plt.ylabel("")
+                    plt.show();
+
+        else:
+            subplots = len(sig_cat_col) # Guardamos las filas que va a tener nuestra figura
+            plt.figure(figsize=(20,15))
+            for i, cat in enumerate(sig_cat_col):
+                plt.subplot(subplots, 1, i+1) # Colocamos cada columna en una de las filas de nuestra figura
                 sns.histplot(x = target_col, hue = cat, data = dataframe, kde = True)
-                plt.show()
+                plt.title(f"Relación entre {cat} y {target_col}")
+                plt.xlabel(cat)
+                plt.ylabel("")
+            plt.tight_layout()
+            plt.show();
