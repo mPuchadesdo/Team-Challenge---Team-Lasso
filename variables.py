62fe25977d45c13d4e3dfd33ae4f6f4ab77efd0b
JBC2024 <jbarcoc@gmail.com>
2025-01-22 17:35:21 +0100

    Funciones y variables
    Se incluye una función para comprobar los parámetros. Y se añaden configuraciones al archivo de variables

--- variables.py
+++ variables.py
@@ -1,5 +1,11 @@
-# Tipos de coluumnas
+COLUMN_TIPO = "tipo"
+COLUMN_NOMBRE = "variable"
+
+# Nombres de las columnas
 TIPO_BINARIA = "Binaria"
 TIPO_CATEGORICA = "Categórica"
 TIPO_NUM_CONTINUA = "Numérica Continua"
 TIPO_NUM_DISCRETA = "Numérica Discreta"
+
+UMBRAL_CATEGORIA = 10 
+UMBRAL_CONTINUA = 15
