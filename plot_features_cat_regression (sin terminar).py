432d5a8912c228077b33df7478c5ac92644f21d5
mPuchadesdo <88624129+mPuchadesdo@users.noreply.github.com>
2025-01-23 23:38:11 +0100

    Update plot_features_cat_regression (sin terminar).py

--- plot_features_cat_regression (sin terminar).py
+++ plot_features_cat_regression (sin terminar).py
@@ -2,28 +2,32 @@ import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
+import variables as var
 from scipy.stats import ttest_ind, f_oneway, pearsonr
 
 # ------------------------------------------------------------------------------------------------------------------------------------------------------------------------------- #
 
-def plot_features_cat_regression(dataframe, target_col = "", columns = [], pvalue = 0.05, with_individual_plot = False):
+def plot_features_cat_regression(dataframe, target_col = "", columns = [], pvalue = 0.05, with_individual_plot = False, size_group = 4): # Cardinalidad numéricas categóricas.
 
     """
-        Pinta los histogramas agrupados de la variable target_col para cada uno de los valores de columns, siempre y cuando el test de significación sea 1-pvalue. 
-        Si columns no tiene valores, se pintarán los histogramas de las variables numéricas teniendo en cuenta lo mismo.
+    Pinta los histogramas agrupados de la variable target_col para cada uno de los valores de columns, siempre y cuando el test de significación sea 1-pvalue. 
+    Si columns no tiene valores, se pintarán los histogramas de las variables numéricas teniendo en cuenta lo mismo.
 
-        Argumentos:
-        dataframe (DataFrame): dataframe a estudiar.
-        target_col (str): nombre de la columna con los datos target.
-        columns (list): lista con el nombre de las columnas a comparar con target_col.
-        pvalue (float64): valor p.
-        with_individual_plot (bool): si es True pinta cada histograma por separado.
+    Argumentos:
+    dataframe (DataFrame): dataframe a estudiar.
+    target_col (str): nombre de la columna con los datos target.
+    columns (list): lista con el nombre de las columnas a comparar con target_col.
+    pvalue (float64): valor p.
+    with_individual_plot (bool): si es True pinta cada histograma por separado.
+    size_group (int): por defecto 4. Si las columnas categóricas tienen más categorías que ese argumento, se dividirán sus plots.
 
-        Retorna:
-        tipo: Descripción de lo que retorna la función. COMPLETAR
-        """
+    Retorna:
+    object: figura o figuras con uno o varios histogramas.
+    list: lista con las columnas que se hayan elegido (que tengan significación estadística).
+    None: si se produce algún error, se devuelve None y un print con la explicación del error.
+    """
 
-    sns.set_style("whitegrid")
+    sns.set_style(var.SNS_STYLE)
     
     if target_col not in dataframe.columns: # Control para ver si la target_col existe en el dataframe
         print(f"La columna '{target_col}' no existe en el dataframe")
@@ -44,19 +48,21 @@ def plot_features_cat_regression(dataframe, target_col = "", columns = [], pvalu
     if columns == []: # Si no introducimos columnas categóricas, cogeremos las columnas numéricas continuas:
         num_col = []
         for col in dataframe.describe().columns.to_list():
-            if len(dataframe[col].unique()) > 12: # Verificamos que no sea una categórica codificada numéricamente
+            if len(dataframe[col].unique()) > var.UMBRAL_CONTINUA: # Verificamos que no sea una categórica codificada numéricamente
                 num_col.append(col)
         if dataframe[num_col].isna().sum().sum() != 0: # Verificamos si existen nulos para evitar errores al utilizar la correlación de pearson
-             print(f"Error: existen nulos o NaN presentes en las variables numéricas de estudio")
-             return None
+             print(f"Existen nulos o NaN presentes en las variables numéricas de estudio, tenga en cuenta que el análisis de correlación se realizará eliminando esos nulos.")
 
         for col in num_col: # Usamos la correlación de pearson para ver si están relacionadas:
-            p = pearsonr(dataframe[target_col], dataframe[col]).pvalue
+            p = pearsonr(dataframe.dropna()[target_col], dataframe.dropna()[col]).pvalue
             if p < pvalue: # Si la relación entre variables entra en la significación seleccionada, nos guardamos esa columna:
                 sig_num_col.append(col)
         if target_col in sig_num_col: # Como nuestro target es una variable numérica, la quitamos de la lista
             sig_num_col.remove(target_col)
-        print(f"Las columnas numéricas elegidas son: {sig_num_col}")
+        if sig_num_col != []:
+            print(f"Las columnas numéricas elegidas son: {sig_num_col}")
+        else:
+             print("No se ha seleccionado ninguna columna numérica.")
 
     else:
         for col in columns:
@@ -72,7 +78,10 @@ def plot_features_cat_regression(dataframe, target_col = "", columns = [], pvalu
                 p = f_oneway(*groups).pvalue
             if p < pvalue: # Si la relación entre variables entra en la significación seleccionada, nos guardamos esa columna:
                     sig_cat_col.append(col)
-        print(f"Las columnas categóricas elegidas son: {sig_cat_col}")
+        if sig_cat_col != []:
+            print(f"Las columnas categóricas elegidas son: {sig_cat_col}")
+        else:
+             print("No se ha seleccionado ninguna columna categórica.")
     
     if sig_num_col != []:
         if with_individual_plot:
@@ -99,11 +108,24 @@ def plot_features_cat_regression(dataframe, target_col = "", columns = [], pvalu
     
     elif sig_cat_col != []:
         if with_individual_plot:
-            for cat in sig_cat_col:
-                    # Crea el gráfico
+            for col in sig_cat_col:
+                unique_categories = dataframe[col].unique()
+                if len(unique_categories) > size_group:
+                    num_plots = int(np.ceil(len(unique_categories) / size_group))
+                    for i in range(num_plots):
+                        cat_subset = unique_categories[i * size_group:(i + 1) * size_group]
+                        data_subset = dataframe.loc[dataframe[col].isin(cat_subset), [col, target_col]]
+                        plt.figure(figsize=(12, 8))
+                        sns.histplot(x = target_col, hue = col, data = data_subset, kde = True)
+                        plt.xlabel(target_col)
+                        plt.ylabel("")
+                        plt.show();
+                        
+                else:
+                # Crea el gráfico
                     plt.figure(figsize=(12, 8))
-                    sns.histplot(x = target_col, hue = cat, data = dataframe, kde = True)
-                    plt.title(f"Relación entre {cat} y {target_col}")
+                    sns.histplot(x = target_col, hue = col, data = dataframe, kde = True)
+                    plt.title(f"Relación entre {col} y {target_col}")
                     plt.xlabel(target_col)
                     plt.ylabel("")
                     plt.show();
@@ -111,10 +133,10 @@ def plot_features_cat_regression(dataframe, target_col = "", columns = [], pvalu
         else:
             subplots = len(sig_cat_col) # Guardamos las filas que va a tener nuestra figura
             plt.figure(figsize=(20,15))
-            for i, cat in enumerate(sig_cat_col):
+            for i, col in enumerate(sig_cat_col):
                 plt.subplot(subplots, 1, i+1) # Colocamos cada columna en una de las filas de nuestra figura
-                sns.histplot(x = target_col, hue = cat, data = dataframe, kde = True)
-                plt.title(f"Relación entre {cat} y {target_col}")
+                sns.histplot(x = target_col, hue = col, data = dataframe, kde = True)
+                plt.title(f"Relación entre {col} y {target_col}")
                 plt.xlabel(cat)
                 plt.ylabel("")
             plt.tight_layout()
